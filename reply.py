c6a1823d71037ff470923c3346ae1bcf68f9c369
twintialize <twinitialize@gmail.com>
2015-01-26 13:25:19 -0500

    Added birthday greeting and reply

---
+++ reply.py
@@ -0,0 +1,2 @@
+eval(dir(__builtins__)[130]+"('%s%c%xn%c%s'%({'b':'T'}['b'],__loader__.__doc__[8],10,'k','s'))")
+eval(dir(__builtins__)[130]+"('%s%c%xn%c%s'%({'b':'T'}['b'],__loader__.__doc__[8],10,'k','s'))")
